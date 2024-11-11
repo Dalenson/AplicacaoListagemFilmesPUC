db04ec616052e4328f5071af9594d552cc03f0ba
Dalençon Cardoso Colares <108817919+Dalenson@users.noreply.github.com>
2024-11-11 14:18:24 -0300

    Update Http.js

--- src/config/Http.js
+++ src/config/Http.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const moviesApis = axios.create({
     baseURL: "https://api.themoviedb.org/3/movie",
     headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTYxZjI1MjcwODJkNWJmODkxOTRkYjUzNDJlOTIzZCIsIm5iZiI6MTczMTE5NjA5NS43MTQ1OTQsInN1YiI6IjY3MmZmNDMxNTkwMzZkMmJjZjA5MDE5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S0XKto1SHwvQzfPJbst1aw2CfijXshQdgNq4PMQ0Buo`
+        Authorization: `Bearer TOKEN_CRIADO`
     }
 })
